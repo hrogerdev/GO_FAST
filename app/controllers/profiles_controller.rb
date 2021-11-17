277763a6150564bf3181f474522bb2b0613a26da
stanislas barrage <stanislas.barrage@gmail.com>
2021-11-17 15:54:37 +0100

    mule and user see the offer

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,10 @@ class ProfilesController < ApplicationController
 
     def show
         @offers = current_user.offers
+        mules = current_user.mules
+        mules.each do |mule|
+            @offers << mule.offers
+        end
 
         @current_user = current_user
         if current_user.mules
