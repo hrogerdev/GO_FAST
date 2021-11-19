f070438e8eec56ab0fd18f4293ac31c5ffa8e52e
hugo roger <hugo.roger@pm.me>
2021-11-19 13:43:51 +0100

    fix latitude in change

--- db/migrate/20211118142913_add_coordinates_to_mules.rb
+++ db/migrate/20211118142913_add_coordinates_to_mules.rb
@@ -2,9 +2,9 @@ class AddCoordinatesToMules < ActiveRecord::Migration[6.0]
   def change
     add_column :mules, :latitude, :float
     add_column :mules, :longitude, :float
-  end
-  Mule.all.each do |mule|
-    mule.geocode
-    mule.save
+    Mule.all.each do |mule|
+      mule.geocode
+      mule.save
+    end
   end
 end
